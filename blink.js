c796c87c3bcfde6b02d2fee4235914b078ec22c7
allen.xiang <allen.xiang@gaiaworks.cn>
2019-12-28 00:05:45 +0800

    一闪一闪亮晶晶

---
+++ blink.js
@@ -0,0 +1,13 @@
+const rpio = require('rpio');
+
+const TARGET_PIN = 11;
+
+rpio.open(TARGET_PIN, rpio.OUTPUT, rpio.LOW);
+
+function blink() {
+	rpio.write(TARGET_PIN, rpio.HIGH);
+	rpio.sleep(1);
+	rpio.write(TARGET_PIN, rpio.LOW);
+}
+
+setInterval(blink, 1000);
---
+++ index.js
@@ -0,0 +1,29 @@
+const Koa = require('koa');
+const Router = require('@koa/router');
+const rpio = require('rpio');
+
+const TARGET_PIN = 11;
+
+rpio.open(TARGET_PIN, rpio.OUTPUT, rpio.LOW);
+
+function blink() {
+	rpio.write(TARGET_PIN, rpio.HIGH);
+	rpio.sleep(1);
+	rpio.write(TARGET_PIN, rpio.LOW);
+}
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+  blink();
+  ctx.body = {
+    success: true
+  }
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(3000, () => console.log('服务正在运行在端口：3000'));
